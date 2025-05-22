fa0b23296d5cfbf3a979081cab334e761517f686
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-05-22 20:18:37 +0300

    Change to using defer instead of DOMContentLoaded

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,20 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let darkMode = false;
-    const textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6'); //selects all elements that need color changes
+let darkMode = false;
+const textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6'); //selects all elements that need color changes
 
-    // click event listener to add toggle dark theme
-    document.getElementById('theme-toggle').addEventListener('click', function() {
-        if (!darkMode) {
-            document.body.classList.add('dark-mode');
-            textElements.forEach(element => {
-                element.style.color = '#ffffff'; //change color for all elements to white
-            });
-        } else {
-            document.body.classList.remove('dark-mode');
-            textElements.forEach(element => {
-                element.style.color = ''; //does the opposite of the above
-            });
-        }
-        darkMode = !darkMode; //toggle dark mode
-    });
+// click event listener to add toggle dark theme
+document.getElementById('theme-toggle').addEventListener('click', function () {
+    if (!darkMode) {
+        document.body.classList.add('dark-mode');
+        textElements.forEach(element => {
+            element.style.color = '#ffffff'; //change color for all elements to white
+        });
+    } else {
+        document.body.classList.remove('dark-mode');
+        textElements.forEach(element => {
+            element.style.color = ''; //does the opposite of the above
+        });
+    }
+    darkMode = !darkMode; //toggle dark mode
 });
