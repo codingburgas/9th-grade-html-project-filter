53fc132a86819eae8390537b618a8c42576b1837
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-05-22 19:28:56 +0300

    Change ids to follow kebab-case convention

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     let darkMode = false;
     const textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6'); //selects all elements that need color changes
-    
+
     // click event listener to add toggle dark theme
-    document.getElementById('themeToggle').addEventListener('click', function() {
+    document.getElementById('theme-toggle').addEventListener('click', function() {
         if (!darkMode) {
             document.body.classList.add('dark-mode');
             textElements.forEach(element => {
