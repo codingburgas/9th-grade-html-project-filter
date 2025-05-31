6e23f24bae515b2571353be0eb863f82ff3eed8a
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-05-31 23:58:27 +0300

    Add color scheme switch button variable for later use

--- js/color-scheme-switch.js
+++ js/color-scheme-switch.js
@@ -1,8 +1,9 @@
+let schemeSwitchButton = document.querySelector('#theme-switch button');
 let schemeSelect = document.querySelector('#theme-switch select');
 let currentSchemeIndex = window.getStoredSchemeIndex();
 schemeSelect.value = currentSchemeIndex;
 
-document.querySelector('#theme-switch button').addEventListener('click', () => {
+schemeSwitchButton.addEventListener('click', () => {
     currentSchemeIndex = (currentSchemeIndex + 1) % window.cssColorSchemes.length;
     document.documentElement.style.colorScheme = window.cssColorSchemes[currentSchemeIndex];
     schemeSelect.value = currentSchemeIndex;
