03494a07501cb38255f4deb05337e494ca6a3280
AGKostov23 <AGKostov23@codingburgas.bg>
2025-06-10 19:36:15 +0100

    Replace text with icon in theme switch (Close #8)

--- js/color-scheme-switch.js
+++ js/color-scheme-switch.js
@@ -3,15 +3,24 @@ let schemeSelect = document.querySelector('#theme-switch select');
 let currentSchemeIndex = window.getStoredSchemeIndex();
 schemeSelect.value = currentSchemeIndex;
 
+function updateButtonEmoji() {
+    let emojis = ['⚙️', '🌙', '☀️'];
+    schemeSwitchButton.textContent = emojis[currentSchemeIndex];
+}
+
+updateButtonEmoji();
+
 schemeSwitchButton.addEventListener('click', () => {
     currentSchemeIndex = (currentSchemeIndex + 1) % window.cssColorSchemes.length;
     document.documentElement.style.colorScheme = window.cssColorSchemes[currentSchemeIndex];
     schemeSelect.value = currentSchemeIndex;
     localStorage.setItem('cssColorSchemeIndex', currentSchemeIndex);
+    updateButtonEmoji();
 });
 
 schemeSelect.addEventListener('change', () => {
     currentSchemeIndex = parseInt(schemeSelect.value);
     document.documentElement.style.colorScheme = window.cssColorSchemes[currentSchemeIndex];
     localStorage.setItem('cssColorSchemeIndex', currentSchemeIndex);
+    updateButtonEmoji();
 });
